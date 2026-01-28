bc9360930c7f8d5e977743fef551610b79f92abb
ACOFA-GROUP <macire.coulibaly@acofagroup.com>
2026-01-28 11:10:37 -0400

    Update auth.py

--- schemas/auth.py
+++ schemas/auth.py
@@ -1,16 +1,19 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
+
 
 class AgentLogin(BaseModel):
     username: str
     password: str
 
+
 class ProducteurLogin(BaseModel):
     telephone: str
     code_pin: str
+
 
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
     user_id: str
     role: str
-    name: str
+    name: str = ""   # ✅ évite le crash si name est None
